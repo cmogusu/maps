7f96e6eb481ffb5c726f6c45a50afd56e57d7ebc
Clive Dev <cmogusu@gmail.com>
2019-01-17 11:42:05 +0300

    Set up testing

---
+++ src/__tests__/components/functions.js
@@ -0,0 +1,13 @@
+//import React from 'react';
+
+/*
+it('runs', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+*/
+
+it( 'It Lives!!',()=>{
+	expect( true ).toEqual( true );
+})
